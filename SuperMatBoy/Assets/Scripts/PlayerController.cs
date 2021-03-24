7031e034a84c4f3f9561cbc03b689f1b34310c03
unknown <thiagovcs@al.insper.edu.br>
2021-03-24 16:36:56 -0300

    Movimentação

---
+++ SuperMatBoy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private int jumps;
+    private Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        jumps = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();  
+
+        if (jumps > 0){
+            Jump();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jumps--;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            jumps = 2;
+        }
+    }
+}
