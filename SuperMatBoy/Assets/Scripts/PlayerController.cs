88325f2a8fe725f24bc1c1f2816486578e70dc69
unknown <thiagovcs@al.insper.edu.br>
2021-03-24 20:28:12 -0300

    levelDesign

--- SuperMatBoy/Assets/Scripts/PlayerController.cs
+++ SuperMatBoy/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,13 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpForce;
     private int jumps;
+    private bool wallJump;
+    private float lastMov;
     private Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
     {
+        lastMov = 0f;
         rigid = GetComponent<Rigidbody2D>();
         jumps = 2;
     }
@@ -21,22 +24,40 @@ public class PlayerController : MonoBehaviour
         Move();  
 
         if (jumps > 0){
+
             Jump();
         }
     }
 
     void Move()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * speed;
+        if(!wallJump){
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * speed;
+            lastMov = movement.x;
+        }        
     }
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && !wallJump)
         {
             rigid.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             jumps--;
+        }
+        else if(Input.GetButtonDown("Jump") && wallJump)
+        {
+            if (lastMov > 0)
+            {
+                rigid.AddForce(new Vector2(jumpForce/1.5f, jumpForce), ForceMode2D.Impulse);
+                jumps--;
+            }
+            else if(lastMov < 0)
+            {
+                rigid.AddForce(new Vector2(-jumpForce/2, jumpForce), ForceMode2D.Impulse);
+                jumps--;
+            }
+            
         }
     }
 
@@ -45,6 +66,17 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.layer == 8)
         {
             jumps = 2;
+            wallJump = false;
+        }
+        if(collision.gameObject.layer == 9)
+        {
+            wallJump = false;
+        }
+        else if(collision.gameObject.layer == 10)
+        {
+            jumps = 2;
+            wallJump = true;
+            lastMov = -lastMov;
         }
     }
 }
