849cf71a8e47e5e55134eb18dee4b76463227fbc
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-03-29 16:15:47 -0300

    added timer

---
+++ SuperMatBoy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿public class GameManager
+{
+    public enum GameState { MENU, GAME, PAUSE, ENDGAME };
+
+    public GameState gameState { get; private set; }
+    public int vidas;
+    public float timeElapsed;
+    private static GameManager _instance;
+
+    public static GameManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new GameManager();
+        }
+
+        return _instance;
+    }
+    private GameManager()
+    {
+        vidas = 3;
+        timeElapsed = 0.0f;
+    }
+
+    public delegate void ChangeStateDelegate();
+    public static ChangeStateDelegate changeStateDelegate;
+
+    public void ChangeState(GameState nextState)
+    {
+        if (nextState == GameState.GAME) Reset();
+        gameState = nextState;
+        changeStateDelegate();
+    }
+
+    private void Reset()
+    {
+        vidas = 3;
+        timeElapsed = 0.0f;
+    }
+}
