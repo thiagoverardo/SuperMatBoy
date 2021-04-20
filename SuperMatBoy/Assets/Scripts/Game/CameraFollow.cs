abf2529ec347a87cfa885b7d3dafc26e0e80268e
unknown <thiagovcs@al.insper.edu.br>
2021-04-20 18:19:10 -0300

    MelhoriasNaCamera&BugFixes

--- SuperMatBoy/Assets/Scripts/Game/CameraFollow.cs
+++ SuperMatBoy/Assets/Scripts/Game/CameraFollow.cs
@@ -10,12 +10,17 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed;
     public Vector3 offset;
     public PlayerController pc;
+    public Vector3 minValues, maxValue;
 
     void LateUpdate()
     {
         if(pc.moving){
             Vector3 desiredPosition = target.position + offset;
-            Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            Vector3 boundPosition = new Vector3(
+                Mathf.Clamp(desiredPosition.x, minValues.x, maxValue.x),
+                Mathf.Clamp(desiredPosition.y, minValues.y, maxValue.y),
+                Mathf.Clamp(desiredPosition.z, minValues.z, maxValue.z));
+            Vector3 smoothedPos = Vector3.Lerp(transform.position, boundPosition, smoothSpeed);
             transform.position = smoothedPos;
         }   
     }
